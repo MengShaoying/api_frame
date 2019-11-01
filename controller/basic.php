aeaa3fa40b83facc0e1bd45fbf9ae655bf6321ed
MengShaoying <mengshaoying@aliyun.com>
2019-11-01 18:07:14 +0800

    项目修改初始提交

---
+++ controller/basic.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * 构成基本功能的controller
+ *
+ * 请求地址总览：
+ * 请求菜单栏信息：
+ * GET /basic/nav
+ * 修改菜单栏信息：
+ * POST /basic/nav
+ * 获取表格信息：
+ * GET /basic/table
+ * 修改表格：
+ * POST /basic/table
+ *
+ * @author MengShaoying <mengshaoying@aliyun.com>
+ */
+
+if_get('/basic/nav', function()
+{
+});
+
+if_post('/basic/nav', function()
+{
+});
+
+if_get('/basic/table', function()
+{
+});
+
+if_post('/basic/table', function()
+{
+});
--- domain/load.php
+++ domain/load.php
@@ -1,3 +1,4 @@
 <?php
 
 include __DIR__.'/autoload.php';
+require_once __DIR__ . '/knowledge/Solution/autoload.php';
